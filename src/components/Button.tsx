4978817e42d33ab42ed7bb9c71c4295e4c8ba3ee
lpatronr <lpatronr@gmail.com>
2022-11-06 23:07:45 -0500

    feat: click animation

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ const Button: FC<Props> = ({ className, children }) => (
   <button
     type="button"
     className={cn(
-      "mx-auto self-start rounded-md bg-red-500 px-4 py-2 font-medium text-white shadow-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-300 sm:mx-0",
+      "mx-auto self-start rounded-md bg-red-500 px-4 py-2 font-medium text-white shadow-md duration-200 ease-in-out hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-300 active:scale-95 active:transform sm:mx-0",
       className
     )}
   >
