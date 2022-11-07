df942a555303954c896c39eca4af503f852453af
lpatronr <lpatronr@gmail.com>
2022-11-06 23:09:36 -0500

    feat: mock data

--- src/pages/_components/AllProducts/mock.ts
+++ src/pages/_components/AllProducts/mock.ts
@@ -68,7 +68,7 @@ export const allCategories = [
 export const mockProducts = [
   {
     id: 1,
-    title: "Lorem ipsum dolor sit amet",
+    title: "Eau de Parfum",
     description:
       "Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquam tortor, vitae aliquam nisl nunc ut nisl.",
     category: "category-1",
@@ -78,7 +78,7 @@ export const mockProducts = [
   },
   {
     id: 2,
-    title: "Lorem ipsum dolor sit amet",
+    title: "Aqua XL Color",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     category: "category-2",
     price: 50,
@@ -87,7 +87,7 @@ export const mockProducts = [
   },
   {
     id: 3,
-    title: "Lorem ipsum dolor sit amet",
+    title: "Jumbo Eye Pencil",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     category: "category-3",
     price: 50,
@@ -95,7 +95,43 @@ export const mockProducts = [
   },
   {
     id: 4,
-    title: "Lorem ipsum dolor sit amet",
+    title: "Ink Crayon",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "category-4",
+    price: 50,
+    image:
+      "https://images.squarespace-cdn.com/content/v1/52a822e8e4b08568b8f859c7/1539071235919-XCU5BA651JWACTROE3TH/IMG_3094+shadows.jpg?format=2500w",
+  },
+  {
+    id: 5,
+    title: "Xtreme Wear Lipstick",
+    description:
+      "Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquam tortor, vitae aliquam nisl nunc ut nisl.",
+    category: "category-1",
+    price: 100,
+    image:
+      "https://www.maybelline.co.in/~/media/mny/en_in/lip/modules/featured%20product%20collage/feb%202020/slm_2.jpg?h=680&w=680&la=en-IN&hash=8380A93D91F025A0FA9990952E67EED63DFF6267",
+  },
+  {
+    id: 6,
+    title: "Zoeva 234 Luxe Crease Brush",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "category-2",
+    price: 50,
+    image:
+      "https://media.istockphoto.com/photos/beauty-swatches-picture-id1135739038?b=1&k=20&m=1135739038&s=612x612&w=0&h=5O2JGij6y4AJlnOnnjillHkn81g-VNmUDEEjoBB_K70=",
+  },
+  {
+    id: 7,
+    title: "Fenty Beauty Match Stix Matte Skinstick",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    category: "category-3",
+    price: 50,
+    image: "https://cdn.shopify.com/s/files/1/0062/7533/4205/products/262_500x.jpg?v=1646945378",
+  },
+  {
+    id: 8,
+    title: "MAC Lipstick",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     category: "category-4",
     price: 50,
